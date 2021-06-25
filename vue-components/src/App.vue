6f84736487ba1f6b14f1f759f251f2510403deb6
sree296 <nivas296@gmail.com>
2021-06-26 00:00:58 +0530

    create base component structure

---
+++ vue-components/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Greetings />
+</template>
+
+<script>
+import Greetings from './components/Greetings.vue'
+export default {
+  name: 'App',
+  components: {
+    Greetings
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-components/src/components/Greetings.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>Hello Sreeni </h1>
+</template>
+<script>
+export default {
+    name : 'Greetings'
+}
+</script>
