97032e8d9ae9a021d82b43b409eb4044b6c28539
sree296 <nivas296@gmail.com>
2021-06-26 22:31:46 +0530

    props from child to parent

---
+++ child-to-parent/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1>This is App component</h1>
+  <button @click="showModal = true">Open Modal</button>
+  <ChildComp v-show="showModal" @close="showModal = false"/>
+</template>
+
+<script>
+import ChildComp from './components/ChildComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ChildComp
+  },
+  data(){
+    return {
+      showModal : false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+button{
+    margin: 0.5rem;
+    cursor: pointer;
+    color: #fff;
+    background-color: #0d6efd;
+    border: 1px solid #0d6efd;
+    font-weight: bold;
+    padding: 0.5rem;
+}
+</style>
---
+++ child-to-parent/src/components/ChildComp.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="modal">
+      <div class="over-lay"></div>
+      <div class="modal-content">
+        <h1>Modal Header</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+        <button @click="$emit('close')">Close</button>
+      </div>
+  </section>
+</template>
+<script>
+export default {
+  name : 'ChildComp',
+  emits : ['close']
+}
+</script>
+<style scoped>
+.over-lay{
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 2;
+    overflow: hidden;
+    outline: 0;
+    background: #00000050;
+}
+.modal-content{
+  background: #fff;
+  max-width: 400px;
+  z-index: 3;
+  position: absolute;
+  padding: 1rem;
+  border-radius: 3px;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
