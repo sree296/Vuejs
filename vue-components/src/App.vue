20b7fbfaf78b5ed123ee533492bcb8ce7f5aed42
sree296 <nivas296@gmail.com>
2021-06-26 08:51:44 +0530

    prop Types

--- vue-components/src/App.vue
+++ vue-components/src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <h1>Hello</h1>
-  <Greetings name="Sree" initial="First Name"/>
+  <!-- <Greetings name="Sree" initial="First Name"/>
   <Greetings name="Nivas" initial="Middle Name"/>
   <Greetings name="Gattu" initial="Last Name"/>
-  <Greetings :name="name" :initial="initial"/>
+  <Greetings :name="name" :initial="initial"/> -->
+
+ <!-- :likescount -> : is for passing count as a Number instead of a string , same with Boolean -->
+  <Blog title='Veu JS Docs' :likesCount="90" :isPublished="true"/>
 </template>
 
 <script>
-import Greetings from './components/Greetings.vue'
+// import Greetings from './components/Greetings.vue'
+import Blog from './components/Blog.vue'
 export default {
   name: 'App',
   components: {
-    Greetings
+    // Greetings
+    Blog
   },
   data(){
     return{
-      name : 'JS',
-      initial : "Vue"
+      // name : 'JS',
+      // initial : "Vue"
     }
   }
 }
---
+++ vue-components/src/components/Blog.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{title}}</h1>
+    <h1>{{likesCount}} Likes</h1>
+    <div>
+        <h2 v-if="isPublished">This Article is Published</h2>
+        <h2 v-else>This Article is Not Published</h2>
+    </div>
+</template>
+<script>
+export default {
+    name : 'Blog',
+    props : {
+      title : String,
+      likesCount : Number,
+      isPublished : Boolean
+    }
+}
+</script>
