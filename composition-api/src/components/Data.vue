94342e636bf11db08ee56b9b93d324106c7d078e
sree296 <nivas296@gmail.com>
2021-06-27 23:17:57 +0530

    replace data() with reactive()

--- composition-api/src/components/Data.vue
+++ composition-api/src/components/Data.vue
@@ -1,17 +1,25 @@
 <template>
   <h2>Options -- {{o_fname}}</h2>
   <h2>Composition -- {{c_fname}} </h2>
+  <h2>Reactive -- {{state.r_fname}} and {{state.r_lname}} Name</h2>
 </template>
 
 <script>
-import {ref} from 'vue';
+import {reactive, ref} from 'vue';
 
 export default {
     name : 'Data',
     setup(){
         const c_fname = ref('Gattu');
+
+        const state = reactive({
+            r_fname : 'Reactive first',
+            r_lname : 'Reactive last'
+        });
+
         return{
-            c_fname
+            c_fname,
+            state
         }
     },
     data(){
