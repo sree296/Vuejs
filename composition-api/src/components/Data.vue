f5427587810fec73f2d4e700718da21418c3bede
sree296 <nivas296@gmail.com>
2021-06-27 23:05:34 +0530

    import ref and include setUp() method

--- composition-api/src/components/Data.vue
+++ composition-api/src/components/Data.vue
@@ -1,10 +1,19 @@
 <template>
   <h2>Options -- {{o_fname}}</h2>
+  <h2>Composition -- {{c_fname}} </h2>
 </template>
 
 <script>
+import {ref} from 'vue';
+
 export default {
     name : 'Data',
+    setup(){
+        const c_fname = ref('Gattu');
+        return{
+            c_fname
+        }
+    },
     data(){
         return{
             o_fname : 'Sreeni'
