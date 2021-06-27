c5ff2789d3e40ebf741b5733f2fda24e290a361c
sree296 <nivas296@gmail.com>
2021-06-27 23:00:44 +0530

    replacing data() using ref

---
+++ composition-api/src/components/Data.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>Options -- {{o_fname}}</h2>
+</template>
+
+<script>
+export default {
+    name : 'Data',
+    data(){
+        return{
+            o_fname : 'Sreeni'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
