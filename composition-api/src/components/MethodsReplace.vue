5cc72833ec59cc1d2fe5d7c9bdcfc5ff8ac5d89a
sree296 <nivas296@gmail.com>
2021-06-27 23:50:44 +0530

    replace methods with a function in setup()

--- composition-api/src/components/MethodsReplace.vue
+++ composition-api/src/components/MethodsReplace.vue
@@ -2,11 +2,25 @@
     <h2>Replacement for Methods</h2>
     <h3>{{userName}} </h3>
     <button @click="setName">Change User Name</button>
+
+    <h2>{{count}}</h2>
+    <button @click="incrementCount">Increment Count</button>
 </template>
 
 <script>
+import {ref} from 'vue';
 export default {
     name : 'MethodsReplace',
+    setup(){
+        const count = ref(0);
+         function incrementCount(){
+            count.value++
+        }
+        return{
+            count,
+            incrementCount
+        }
+    },
     data(){
         return{
             userName : 'Sreeni'
