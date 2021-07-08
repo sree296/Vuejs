a77e333c7f272294ff7c5ec55831d6ecdcd3cc49
sree296 <nivas296@gmail.com>
2021-07-09 00:04:50 +0530

    basic methods in vue

---
+++ methods-in-vue/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+<fieldset>
+    <legend>Methods in Vue</legend>
+    <MethodsComponent/>
+</fieldset>
+  
+</template>
+
+<script>
+import MethodsComponent from './components/MethodsComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MethodsComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  max-width: 750px;
+  margin: 0 auto;
+}
+button{
+    margin: 0.5rem;
+    cursor: pointer;
+    color: #fff;
+    background-color: #0d6efd;
+    border: 1px solid #0d6efd;
+    font-weight: bold;
+    padding: 0.5rem;
+}
+</style>
---
+++ methods-in-vue/src/components/MethodsComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <h2>{{fname}}</h2>
+  <button @mousemove="changeName('Arg - Gattu')"> Change Name </button>
+
+  <h3>Number : {{numVal}} </h3>
+  <button @click="increment(2)">Increment By 2</button>
+  <button @click="decrement(1)">Decrement By 1</button>
+</template>
+<script>
+export default {
+  name : 'MethodsComponent',
+  data(){
+    return{
+      fname : 'Sreeni',
+      numVal : 0
+    }
+  },
+  methods : {
+    changeName(val){
+      this.fname = val;
+    },
+    increment(incVal){
+      this.numVal += incVal;
+    },
+    decrement(decVal){
+      this.numVal -= decVal;
+    }
+  }
+}
+</script>
